515884a03b215cd17cfc343389dd63efee6a45ba
Shazzadhossensunny <shazzadhossensunny@gmail.com>
2025-01-13 21:45:13 +0600

    send every req access token

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,10 +1,36 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { RootState } from "../store";
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: "http://localhost:5000/api/v1",
+  credentials: "include",
+  prepareHeaders: (headers, { getState }) => {
+    const token = (getState() as RootState).auth.token;
+    if (token) {
+      headers.set("authorization", `${token}`);
+    }
+    return headers;
+  },
+});
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1",
-    credentials: "include",
-  }),
+  baseQuery: baseQuery,
   endpoints: () => ({}),
 });
+
+/*
+prepareHeaders: (headers, { getState }) => {
+      const token = getState().auth.token;
+      console.log(token);
+
+      console.log(getState());
+      console.log("HEADERS", headers);
+
+      if (token) {
+        headers.set("Authorization", `${token}`);
+      }
+      return headers;
+    },
+
+*/
