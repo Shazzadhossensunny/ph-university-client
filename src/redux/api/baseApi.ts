80a0114c3557a53a7c6809c88ec8504d20815c9c
Shazzadhossensunny <shazzadhossensunny@gmail.com>
2025-01-14 16:52:38 +0600

    accessToken expried then again refreshToken get the token

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,11 +1,18 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
 
+import type {
+  BaseQueryFn,
+  FetchArgs,
+  FetchBaseQueryError,
+} from "@reduxjs/toolkit/query";
+import { logout, setUser } from "../features/auth/authSlice";
+
 const baseQuery = fetchBaseQuery({
   baseUrl: "http://localhost:5000/api/v1",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
-    const token = (getState() as RootState).auth.token;
+    const token = (getState() as RootState).auth?.token;
     if (token) {
       headers.set("authorization", `${token}`);
     }
@@ -13,24 +20,39 @@ const baseQuery = fetchBaseQuery({
   },
 });
 
+const baseQueryWithRefreshToken: BaseQueryFn<
+  string | FetchArgs,
+  unknown,
+  FetchBaseQueryError
+> = async (args, api, extraOptions) => {
+  let result = await baseQuery(args, api, extraOptions);
+  //* Check if the request was unauthorized
+  if (result?.error && result?.error?.status === 401) {
+    const res = await fetch("http://localhost:5000/api/v1/auth/refresh-token", {
+      method: "POST",
+      credentials: "include",
+    });
+    const refreshData = await res.json();
+    console.log(refreshData);
+    if (refreshData.data) {
+      const user = (api.getState() as RootState).auth.name;
+      console.log(user);
+      api.dispatch(
+        setUser({
+          user,
+          token: refreshData.data.accessToken,
+        })
+      );
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      api.dispatch(logout());
+    }
+  }
+  return result;
+};
+
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: baseQuery,
+  baseQuery: baseQueryWithRefreshToken,
   endpoints: () => ({}),
 });
-
-/*
-prepareHeaders: (headers, { getState }) => {
-      const token = getState().auth.token;
-      console.log(token);
-
-      console.log(getState());
-      console.log("HEADERS", headers);
-
-      if (token) {
-        headers.set("Authorization", `${token}`);
-      }
-      return headers;
-    },
-
-*/
