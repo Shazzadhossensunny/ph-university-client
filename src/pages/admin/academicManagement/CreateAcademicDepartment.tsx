93b90df0dbc4542116c4801cc3cc1a9e030f5652
Shazzadhossensunny <shazzadhossensunny@gmail.com>
2025-01-16 22:31:20 +0600

    academic department form create

--- src/pages/admin/academicManagement/CreateAcademicDepartment.tsx
+++ src/pages/admin/academicManagement/CreateAcademicDepartment.tsx
@@ -1,3 +1,20 @@
+import { FieldValues } from "react-hook-form";
+import PHForm from "../../../components/form/PHForm";
+import { Button, Col, Row } from "antd";
+import PHSelect from "../../../components/form/PHSelect";
+
 export default function CreateAcademicDepartment() {
-  return <div>CreateAcademicDepartment</div>;
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
+  };
+  return (
+    <Row justify="center" align="middle" style={{ height: "100vh" }}>
+      <Col span={6}>
+        <PHForm onSubmit={onSubmit}>
+          <PHSelect label="Name" name="name" options={"hello"} />
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Col>
+    </Row>
+  );
 }
